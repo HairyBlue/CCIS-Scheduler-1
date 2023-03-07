48cc5181ef07dedc04c02599b1e04ff56d6673db
Edmund Nillas III <edmundnillas3rd@gmail.com>
2023-03-07 20:06:33 +0800

    Add responsive designs to each components

--- web/src/components/StudentPage/Schedule.jsx
+++ web/src/components/StudentPage/Schedule.jsx
@@ -5,12 +5,12 @@ import "./Schedule.css";
 export default function Schedule() {
   return (
     <div className="container schedule">
-      <div className="container schedule-header">
+      <div className="schedule-header">
         <p>Schedule for this Week</p>
         <div className="divider"></div>
       </div>
 
-      <div className="container schedule-meetings-container">
+      <div className="schedule-meetings-container">
         <MeetingCard />
         <MeetingCard />
         <MeetingCard />
