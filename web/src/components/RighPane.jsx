c902379f476328652722491be9fc9a6f3953a23a
Edmund Nillas III <edmundnillas3rd@gmail.com>
2023-03-30 09:52:18 +0800

    Add meeting cards for displaying upcoming meetings

--- web/src/components/RighPane.jsx
+++ web/src/components/RighPane.jsx
@@ -1,4 +1,35 @@
+import { useEffect, useState } from "react";
 import "./RightPane.css";
+
+const MeetingCard = ({ title, description }) => {
+  const [time, setTime] = useState(0);
+
+  useEffect(() => {
+    setTime(2);
+  }, []);
+
+  return (
+    <div className="meeting-card-container container">
+      <div className="leftpane-card container column">
+        <p>{title}</p>
+        <p>{description}</p>
+        <div className="time-tracker-container">
+          <div className="img-container">
+            <img
+              src="https://cdn-icons-png.flaticon.com/512/2088/2088617.png"
+              alt="clock-icon"
+            />
+          </div>
+          <div>{`${time} hr`}</div>
+        </div>
+      </div>
+      <div className="rightpane-card container">
+        <div className="status-indicator-container"></div>
+        <p className="status-container">New</p>
+      </div>
+    </div>
+  );
+};
 
 export default function RightPane() {
   const weekday = [
@@ -48,7 +79,56 @@ export default function RightPane() {
           <button>New Meeting</button>
         </div>
       </div>
-      <div className="rightpane-container border-style"></div>
+      <div className="rightpane-content-container container column border-style flex">
+        <MeetingCard
+          title={"Meeting Name"}
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eleifend luctus fringilla. Sed in eros velit. Phasellus sed aliquet eros. Fusce ut orci convallis risus auctor venenatis nec at augue."
+        />
+        <MeetingCard
+          title={"Meeting Name"}
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eleifend luctus fringilla. Sed in eros velit. Phasellus sed aliquet eros. Fusce ut orci convallis risus auctor venenatis nec at augue."
+        />
+        <MeetingCard
+          title={"Meeting Name"}
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eleifend luctus fringilla. Sed in eros velit. Phasellus sed aliquet eros. Fusce ut orci convallis risus auctor venenatis nec at augue."
+        />
+        <MeetingCard
+          title={"Meeting Name"}
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eleifend luctus fringilla. Sed in eros velit. Phasellus sed aliquet eros. Fusce ut orci convallis risus auctor venenatis nec at augue."
+        />
+        <MeetingCard
+          title={"Meeting Name"}
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eleifend luctus fringilla. Sed in eros velit. Phasellus sed aliquet eros. Fusce ut orci convallis risus auctor venenatis nec at augue."
+        />
+        <MeetingCard
+          title={"Meeting Name"}
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eleifend luctus fringilla. Sed in eros velit. Phasellus sed aliquet eros. Fusce ut orci convallis risus auctor venenatis nec at augue."
+        />
+        <MeetingCard
+          title={"Meeting Name"}
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eleifend luctus fringilla. Sed in eros velit. Phasellus sed aliquet eros. Fusce ut orci convallis risus auctor venenatis nec at augue."
+        />
+        <MeetingCard
+          title={"Meeting Name"}
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eleifend luctus fringilla. Sed in eros velit. Phasellus sed aliquet eros. Fusce ut orci convallis risus auctor venenatis nec at augue."
+        />
+        <MeetingCard
+          title={"Meeting Name"}
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eleifend luctus fringilla. Sed in eros velit. Phasellus sed aliquet eros. Fusce ut orci convallis risus auctor venenatis nec at augue."
+        />
+        <MeetingCard
+          title={"Meeting Name"}
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eleifend luctus fringilla. Sed in eros velit. Phasellus sed aliquet eros. Fusce ut orci convallis risus auctor venenatis nec at augue."
+        />
+        <MeetingCard
+          title={"Meeting Name"}
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eleifend luctus fringilla. Sed in eros velit. Phasellus sed aliquet eros. Fusce ut orci convallis risus auctor venenatis nec at augue."
+        />
+        <MeetingCard
+          title={"Meeting Name"}
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eleifend luctus fringilla. Sed in eros velit. Phasellus sed aliquet eros. Fusce ut orci convallis risus auctor venenatis nec at augue."
+        />
+      </div>
     </div>
   );
 }
