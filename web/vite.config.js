2fc118a464540b786b11d852fcef5c8e7a88de8b
Edmund Nillas III <edmundnillas3rd@gmail.com>
2023-04-07 07:40:27 +0800

    Revise vite config

--- web/vite.config.js
+++ web/vite.config.js
@@ -1,13 +1,8 @@
+import { resolve } from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    manifest: true,
-    rollupOptions: {
-      input: "./src/main.jsx"
-    }
-  }
+  plugins: [react()]
 });
