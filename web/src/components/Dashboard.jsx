aa1a7fccd102eb37d4ccea8389b6f3ab95df6197
Edmund Nillas III <edmundnillas3rd@gmail.com>
2023-04-08 07:52:50 +0800

    Add responsive styling for dashboard component

--- web/src/components/Dashboard.jsx
+++ web/src/components/Dashboard.jsx
@@ -1,11 +1,12 @@
 import LeftPane from "./LeftPane";
 import RightPane from "./RightPane";
+import "./Dashboard.css";
 
 export default function Dashboard() {
   return (
-    <>
+    <div className="dashboard-container container flex">
       <LeftPane />
       <RightPane />
-    </>
+    </div>
   );
 }
