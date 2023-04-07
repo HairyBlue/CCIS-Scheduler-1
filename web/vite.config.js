15a2004cb9f9cf30fd63545f53249a59ec92a8c2
Edmund Nillas III <edmundnillas3rd@gmail.com>
2023-04-07 20:23:28 +0800

    Add new configurations for production build

--- web/vite.config.js
+++ web/vite.config.js
@@ -1,8 +1,23 @@
-import { resolve } from "path";
+import { fileURLToPath } from "url";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url))
+    }
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        app: "./index.html"
+      }
+    }
+  },
+  server: {
+    open: "/index.html"
+  }
 });
